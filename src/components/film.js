9606b874736d586fd367b17071bc3ab8c1ffd650
Ronan Hughes <rhughes490@gmail.com>
2020-09-14 23:32:25 +0100

    MVP complete

---
+++ src/components/film.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Film extends Component {
+  render() { 
+    return (
+      <li>
+        <a href={this.props.url}>
+          {this.props.name}
+        </a>
+      </li>
+    );
+  }
+}
+ 
+export default Film;
